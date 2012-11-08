6af55ab330bf9e0bea447dcd0eff56930557f173
corcorans <corcorans13@gmail.com>
2012-11-08 09:10:02 -0600

    Changed template for the score view in the homes file. Added an edit and delete button for each player record, also an add button was create along with the fields needed when creating new player information.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,21 @@
 class HomesController < ApplicationController
+    def create
+      # Valid
+      # Create new player with data in db
+      # show on page
+    end
+ 
+    def update
+      # Link to new page to change data
+      # validate
+      # submit and return to homes#index
+    end
+
+
+    def destroy
+      # Get player name 
+      # Search db for id or name
+      # display msg are you sure?
+      # remove from db and page
+    end
 end
