b164f448ab717400e06fc9a8b991c13f3cfbf6c9
corcorans <corcorans13@gmail.com>
2012-12-10 20:10:26 -0600

    Added sql call in the statistics controller so that the average can be calculated for each course. Player index was modified to add a button to the statistics page.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,8 +1,5 @@
 class StatisticsController < ApplicationController
-  def index
-    # Get course names
-    # sort alphabetically
-    # Get stats
-    # Average them
+  def create
+    @total = Player.group(:course_name).sum(:score)
   end
 end
